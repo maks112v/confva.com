c759de5c4a9f8249797a9bdcd12bee423205f2da
Mark Pekun <mark.pekun@gmail.com>
2022-11-23 23:30:07 -0500

    new link to q&a

--- apps/conf-2022/src/components/Hero.jsx
+++ apps/conf-2022/src/components/Hero.jsx
@@ -39,10 +39,10 @@ export function Hero() {
           </div>
           <Button
             target="_blank"
-            href="https://docs.google.com/forms/d/e/1FAIpQLSf7ug3QLXJ6G2gOqv6Udhi6W_o159yFdRcQNOh_fOaFpQNxvw/viewform"
+            href="https://forms.gle/PrB2zmxuW72rSR8f7"
             className="w-full mt-10 sm:hidden"
           >
-            Sign Up
+            Submit Question
           </Button>
           <dl className="grid grid-cols-2 mt-10 gap-y-6 gap-x-10 sm:mt-16 sm:gap-y-10 sm:gap-x-16 sm:text-center lg:auto-cols-auto lg:grid-flow-col lg:grid-cols-none lg:justify-start lg:text-left">
             {[
